83a2289d21530aef2d837e04a1517d061a951ebc
offsetvoid <74632982+offsetvoid@users.noreply.github.com>
2022-08-02 16:53:34 -0700

    Update games supported.lua

--- games supported.lua
+++ games supported.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 8750997647 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/offsetvoid/Hub/main/tapping%20legends.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/offsetvoid/Hub/main/Protected.lua",true))()
 end
 
 if game.PlaceId == 10325366821 then
