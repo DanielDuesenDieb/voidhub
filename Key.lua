83944b0e6be19a0485b62e9402200dd253d1956b
offsetvoid <74632982+offsetvoid@users.noreply.github.com>
2022-08-02 15:21:24 -0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -15,7 +15,7 @@ _G.KeyInput = "string"
 function MakeScriptHub()
     local Window1 =
     OrionLib:MakeWindow({ Name = "Script Hub", HidePremium = false, SaveConfig = true, IntroText = "Script Hub" })
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/offsetvoid/Hub/main/games%20supported.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/offsetvoid/Hub/main/games%20supported.lua", true))()
 end
 
 function CorrectKeyNotification()
