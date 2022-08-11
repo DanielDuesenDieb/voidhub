133fed8ead1f05f92a1588039bc381a87cf3b5a0
offsetvoid <74632982+offsetvoid@users.noreply.github.com>
2022-08-11 12:52:29 -0700

    Update games supported.lua

--- games supported.lua
+++ games supported.lua
@@ -1,17 +1,18 @@
-if game.PlaceId == 8750997647 then
+if game.PlaceId == 8750997647 then--Tapping Legends
     loadstring(game:HttpGet("https://raw.githubusercontent.com/offsetvoid/voidhub/main/tapping%20legends.lua",true))()
 end
 
-if game.PlaceId == 155615604 then 
+if game.PlaceId == 155615604 then --Prison Life
     loadstring(game:HttpGet("https://raw.githubusercontent.com/offsetvoid/voidhub/main/prison%20life.lua",true))()
 end
 
-if game.PlaceId == 7127407851 then --AOT (soon)
+if game.PlaceId == 7127407851 then
 end
 
-if game.PlaceId == 3652625463 then --Lifting Sim (soon)
+if game.PlaceId == 3652625463 then --Lifting Sim
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/offsetvoid/voidhub/main/lifting%20sim.lua",true))()
 end
 
-if game.PlaceId == 10515724474 then
+if game.PlaceId == 10515724474 then --Tapping Legends X
     loadstring(game:HttpGet("https://raw.githubusercontent.com/offsetvoid/voidhub/main/tapping%20legends%20x.lua",true))()
 end
