4f4e984f9c62f34489fbee79230442b9e110e11e
offsetvoid <74632982+offsetvoid@users.noreply.github.com>
2022-08-16 15:51:07 -0700

    oops (not really)

--- assassin.lua
+++
@@ -1,7 +0,0 @@
-if game.PlaceId == 379614936 then
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-    local Window = OrionLib:MakeWindow({Name = "voidhub | Assassin!", HidePremium = false, IntroEnabled = false,IntroText = "voidhub", SaveConfig = true, ConfigFolder = "OrionTest"})
-
-    --Values
-    --Tabs
-    --
